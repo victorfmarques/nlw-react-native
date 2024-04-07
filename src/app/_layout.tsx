28e597ff2a843dbf43a3330dd57872e371a6e417
Victor Felisbino Marques <victorfmarques@outlook.com>
2024-04-07 15:03:32 -0300

    finalização do projeto

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar'
-import '../styles/global.css'
+import "@/styles/global.css"
 
-import { Slot } from 'expo-router'
+import { Slot } from "expo-router"
+
+import { Loading } from "@/components/loading"
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto"
 
 export default function Layout() {
-  return <>
-    <StatusBar style='light' />
-    <Slot />
-  </>
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return <Slot />
 }
